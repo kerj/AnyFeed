862f45712c4a3f5458c306dbdcb6f79bdf119e67
kerj <kerntzj@gmail.com>
2025-06-18 14:34:04 -0700

    add string formatters for ease of use

--- iFinesse/Models/utility.swift
+++ iFinesse/Models/utility.swift
@@ -10,3 +10,23 @@ extension Optional {
         self ?? defaultValue
     }
 }
+// String formmating
+func formatDistance(_ meters: Double) -> String {
+    let km = meters / 1000
+    return String(format: "%.2f km", km)
+}
+
+func formatElevation(_ meters: Double) -> String {
+    return String(format: "%.0f m", meters)
+}
+
+func formatDuration(_ seconds: Int) -> String {
+    let hrs = seconds / 3600
+    let mins = (seconds % 3600) / 60
+    let secs = seconds % 60
+    if hrs > 0 {
+        return String(format: "%dh %02dm %02ds", hrs, mins, secs)
+    } else {
+        return String(format: "%dm %02ds", mins, secs)
+    }
+}
